f039ee69659338c4c75f1744d3b5f1ab6450f502
youngwoobae <ywb1122@naver.com>
2023-04-07 16:02:10 +0900

    작업 내용 프쉬

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,72 +1,82 @@
-import { useRef, useState } from 'react';
-const DiaryEditor = () => {
+import { useState, useRef } from "react";
+
+const DiaryEditor = ({ onCreate }) => {
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+        emotion: 1
+    });
 
     const authorInput = useRef();
     const contentInput = useRef();
 
-    const [state, setState] = useState({
-        author: "",
-        content: "",
-        emotion: 1,
-        });
-
-    const handleChangeState = (e)=>{
+    const handleChangeState = (e) => {
         setState({
-        ...state,
-        [e.target.name]: e.target.value,
+            ...state,
+            [e.target.name]: e.target.value
         });
     };
 
-    const handleSubmit = ()=>{
-        if(state.author.length < 1){
+    const handleSubmit = () => {
+        if (state.author.length < 1) {
             authorInput.current.focus();
             return;
         }
-        
-        if(state.content.length < 5){
+
+        if (state.content.length < 5) {
             contentInput.current.focus();
             return;
         }
-        
-        alert("저장 성공!");
-    }
 
-    return ( // code by byw 0316
+        onCreate(state.author, state.content, state.emotion);
+        alert("저장 성공!");
+        setState({
+            author: "",
+            content: "",
+            emotion: 1,
+        });
+    };
+
+    return (
         <div className="DiaryEditor">
-        <h2>오늘의 일기</h2>
-        <div>
-            <input 
-            ref={authorInput}
-            name='author'
-            value={state.author}
-            onChange={handleChangeState}
-            />
-        </div>
-        <div>
-            <textarea 
-            ref={contentInput}
-            name='content'
-            value={state.content}
-            onChange={handleChangeState}
-            />
-        </div>
-        <div>
-            <span>오늘의 감정점수 : </span>
-            <select
-            name="emotion"
-            value={state.emotion}
-            onChange={handleChangeState}
-            >
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            </select>
-        </div>
-        <div>
-            <button onClick={handleSubmit}>일기 저장하기</button>
-        </div>
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    ref={authorInput}
+                    value={state.author}
+                    onChange={handleChangeState}
+                    name="author"
+                    placeholder="작성자"
+                    type="text"
+                />
+            </div>
+            <div>
+                <textarea
+                    ref={contentInput}
+                    value={state.content}
+                    onChange={handleChangeState}
+                    name="content"
+                    placeholder="일기"
+                    type="text"
+                />
+            </div>
+            <div>
+                <span>오늘의 감정점수 : </span>
+                <select
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleChangeState}
+                >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>일기 저장하기</button>
+            </div>
         </div>
     );
 };
---
+++ src/DiaryItem.js
@@ -0,0 +1,32 @@
+const DiaryItem = ({
+  onDelete,
+  id,
+  author,
+  content,
+  emotion,
+  created_date,
+}) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleDateString()}</span>
+      </div>
+      <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기</button>
+    </div>
+  );
+}
+
+export default DiaryItem;
